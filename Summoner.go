e58e5042d236788332ba6b4471017680d3f976e8
tot0p <thomas.lemaitre.78420@gmail.com>
2024-10-18 17:04:17 +0200

    add comment in summoner.go

--- Summoner.go
+++ Summoner.go
@@ -2,6 +2,7 @@ package riot_wrapper
 
 import "ElderLab/riot-wrapper/internal/request"
 
+// GetSummonerByPUUID is a function that returns a summoner by PUUID.
 func (cli *RiotClient) GetSummonerByPUUID(puuid string) (string, []error) {
 	result, err := cli.lolClient.Get("/lol/summoner/v4/summoners/by-puuid/:puuid", []request.ParamURL{
 		{
@@ -15,6 +16,7 @@ func (cli *RiotClient) GetSummonerByPUUID(puuid string) (string, []error) {
 	return string(result), nil
 }
 
+// GetSummonerByAccountId is a function that returns a summoner by AccountId.
 func (cli *RiotClient) GetSummonerByAccountId(accountId string) (string, []error) {
 	result, err := cli.lolClient.Get("/lol/summoner/v4/summoners/by-account/:accountId", []request.ParamURL{
 		{
